99859a6fa94147aea3f0461a330de8d3151b043a
ViníciosGabriel1 <107072932+ViniciosGabriel1@users.noreply.github.com>
2023-12-14 20:31:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
 
     <link href="css/custom.css" rel="stylesheet">
-    <title>Celke - FullCalendar</title>
+    <title>FullCalendar</title>
 </head>
 
 <body>
